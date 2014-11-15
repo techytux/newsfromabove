8963820b897319454fef6e02f9c6ea2d7f81872e
Vaibhav Singh <techvaibhav@gmail.com>
2014-11-16 00:36:03 +0100

    final commit before going home

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -12,18 +12,6 @@ angular.module('myApp.video', ['ngRoute', 'ngVideo', 'ui.bootstrap', 'youtube-em
 .controller('ArticleController', ['$scope', '$http', '$routeParams', 'video', '$rootScope', function($scope, $http, $routeParams, video, $rootScope) {
         var searchParam = $routeParams.search;
 
-        
-
-        //If we need to retrieve the article images
-//        var imageUrl="https://ipool.s.asideas.de:443/api/v3/object/" + identifier + "?encodeBase64=true";
-
-        //If we need video
-//        var video_content = "https://ipool.s.asideas.de/api/v3/search?limit=5&sources=\"escenic\"&types=\"video\"&q=" + $scope.query;
-
-        
-
-        
-
         var makeRequest = function (query) {
             var _self = this;
             var requestUrl = "https://ipool.s.asideas.de/api/v3/search?limit=3&sources=\"escenic\"&types=\"article\"&publisher=\"www.welt.de\"&q=" + query;
@@ -47,62 +35,64 @@ angular.module('myApp.video', ['ngRoute', 'ngVideo', 'ui.bootstrap', 'youtube-em
             $http.get(requestUrl, {
                 headers: { Authorization : authorization['Authorization'] }
             }).success(function(data, status, headers, config) {
-
+                console.log('Request sucess ' + query);
                 $scope.articles = data['documents'];
 
             }).error(function(data, status, headers, config) {
                 $scope.result = "An error occured: " + data;
             });
         };
+        var m = 'videos/Altstadt_Potsdam_UAV.webm'
+
         
-
-
         var video_list = [{
             'name': 'video1',
-            'src': 'videos/Altstadt_Potsdam_UAV.webm',
-            'keyword': 'potsdam'
+            'src': 'videos/Glienicker_Bruecke_Potsdam_UAV_2.webmhd.webm',
+            'keyword': 'Glienicker',
+            'relatedVideos': [{
+                    title: 'Tausche Ostagent gegen Westagent',
+                    id: 'hDwvDHnFWI0'
+                },
+                {
+                    title: 'Potsdam Glienicker Brücke',
+                    id: 'IBN418lPt4M'
+                }
+            ]
         }, {
             'name': 'video2',
-            'src': 'videos/Glienicker_Bruecke_Potsdam_UAV.webm',
-            'keyword': 'Glienicker'
-        }];
-
-
-        $scope.videosBrucke = [
-            {
-                title: 'Tausche Ostagent gegen Westagent',
-                id: 'hDwvDHnFWI0'
-            },
-            {
-                title: 'Potsdam Glienicker Brücke',
-                id: 'IBN418lPt4M'
-            }
-        ];
-
-        $scope.parkBabelsbergs = [
-            {
-                title: 'Park Babelsberg, flying over the Kindermann See (Potsdam, Germany)',
-                id: 'v_upHLPHUP0'
-            }
-        ];
-
-        $scope.alterLandtags = [
-            {
+            'src': 'videos/Altstadt_Potsdam_UAV_2.webmhd.webm',
+            'keyword': 'Altstadt Potsdam',
+            'relatedVideos': [{
+                    title: 'Reichsarchiv POTSDAM Kriegsschule Landtag',
+                    id: 'cpBFFPWUlsc'
+                }
+            ]
+        }, {
+            'name': 'video3',
+            'src': 'videos/Alter_Landtag_Potsdam_UAV_2.webm',
+            'keyword': 'Alter Landtag',
+            'relatedVideos': [{
                 title: 'Reichsarchiv POTSDAM Kriegsschule Landtag',
                 id: 'cpBFFPWUlsc'
             }
-        ];
 
-        $scope.alterLandtags = [
-            {
-                title: 'Der neue Brandenburger Landtag in Potsdam',
-                id: 'u40Dx82N3OI'
-            },
-            {
-                title: 'Am 18.01.2014 fand die feierliche Einweihung des neuen Landtages in Potsdam statt.',
-                id: 'A1jOcHwOnuM'
-            }
-        ];
+            ]
+
+        }, {
+            'name': 'video4',
+            'src': 'videos/Sanssoucis_2_UAV.webmhd.webm',
+            'keyword': 'schloss sanssouci',
+            'relatedVideos': [
+            ]
+
+        }, {
+            'name': 'video5',
+            'src': 'videos/Villa_Schoeningen_UAV_2.webmhd.webm',
+            'keyword': 'Villa Schoeningen',
+            'relatedVideos': [
+            ]
+
+        }];
 
         /**
          * @property playlistOpen
@@ -120,16 +110,17 @@ angular.module('myApp.video', ['ngRoute', 'ngVideo', 'ui.bootstrap', 'youtube-em
             second: video_list[1].src
         };
 
-        new makeRequest(video_list[0].keyword);
-
         $rootScope.videoCallback = function (index) {
             new makeRequest(video_list[index].keyword);
+            $scope.relatedVideos = video_list[index].relatedVideos;
         }
         // Add some video sources for the player!
         video.addSource('webm', video_list[0].src);
-        new makeRequest(video_list[0].keyword);
-
+        $rootScope.videoCallback(0);
         video.addSource('webm', video_list[1].src);
+        video.addSource('webm', video_list[2].src);
+        video.addSource('webm', video_list[3].src);
+        video.addSource('webm', video_list[4].src);
 
         $scope.$watch(function() {
           return video;
