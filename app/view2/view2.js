2179197809c399cf695168b961975a99f57b888f
jmilanez <josiane.milanez@asideas.de>
2014-11-15 16:12:28 +0100

    Adding the request for related articles

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,41 @@ angular.module('myApp.video', ['ngRoute'])
   });
 }])
 
-.controller('ArticleController', [function() {
+.controller('ArticleController', ['$scope', '$http', function($scope, $http) {
+        $scope.query ="Sanssouci";
 
-}]);
+        this.oauth = new OAuth({
+            consumer: {
+                public: "hackathon",
+                secret: "33f3fb11-59b4-4a38-a549-27bb4628d1af"
+            }
+        });
+
+        //If we need to retrieve the article images
+//        var imageUrl="https://ipool.s.asideas.de:443/api/v3/object/" + identifier + "?encodeBase64=true";
+
+        //If we need video
+//        var video_content = "https://ipool.s.asideas.de/api/v3/search?limit=5&sources=\"escenic\"&types=\"video\"&q=" + $scope.query;
+
+        var requestUrl = "https://ipool.s.asideas.de/api/v3/search?limit=5&sources=\"escenic\"&types=\"article\"&q=" + $scope.query;
+
+        var request_data = {
+            url: requestUrl,
+            method: 'GET',
+            data: {}
+        };
+
+        var oauthData = this.oauth.authorize(request_data);
+
+        var authorization = this.oauth.toHeader(oauthData);
+
+        $http.get(requestUrl, {
+            headers: { Authorization : authorization['Authorization'] }
+        }).success(function(data, status, headers, config) {
+
+            $scope.articles = data['documents'];
+
+        }).error(function(data, status, headers, config) {
+            $scope.result = "An error occured: " + data;
+        })
+ }]);
